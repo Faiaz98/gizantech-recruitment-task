cdde51c86381880125dd93bf4102d0c39cf7911a
Md. Iram Faiaz Khan <76048711+Faiaz98@users.noreply.github.com>
2025-06-17 22:01:33 +0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     server: {
-        host: '0.0.0.0', // ✅ This is what makes it work with k6
+        host: '0.0.0.0', // This is what makes it work with k6
         port: 5173 // Optional: to fix the port
     }
 });
